083d0f56951e54266a02cd475215660c4f496888
andreaspwu <lhart7077@gmail.com>
2023-03-05 11:47:48 +0700

    fix problems

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,15 @@ import '../styles/globals.css'
 
 export default function RootLayout({
   children,
-  pageTitle,
-  metaDescription,
 }: {
   children: React.ReactNode
-}): Promise<ReactNode> {
+}) {
   return (
     <html lang="en">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <title>{pageTitle}</title>
-      <meta name="description" content={metaDescription} />
       <head />
       <body className="max-w-6xl mx-auto my-8 p-5">{children}</body>
     </html>
